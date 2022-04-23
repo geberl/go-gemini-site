d9f88b457613e37731169af41ef6c0c6cf3cd7ac
Günther Eberl <guenther.eberl@hetzner-cloud.de>
2022-04-23 17:44:22 +0000

    Index: Tweaks

--- cmd/handler_index.go
+++ cmd/handler_index.go
@@ -12,7 +12,7 @@ func HandlerIndex(baseUrl string, logger log.Logger) func(context.Context, gemin
 	return func(ctx context.Context, w gemini.ResponseWriter, r *gemini.Request) {
 		var text gemini.Text
 
-		text = append(text, gemini.LineHeading1("Main\n"))
+		text = append(text, gemini.LineHeading1("Eberl.se\n"))
 		text = append(text, gemini.LineText("This is my Gemini site. Very much work-in-progress.\n"))
 		text = append(text, gemini.LineLink{
 			URL:  "https://github.com/geberl/go-gemini-site",
@@ -21,7 +21,7 @@ func HandlerIndex(baseUrl string, logger log.Logger) func(context.Context, gemin
 		text = append(text, gemini.LineText(""))
 		text = append(text, gemini.LineLink{
 			URL:  "https://eberl.se",
-			Name: "My main website is eberl.se",
+			Name: "My website",
 		})
 		text = append(text, gemini.LineText(""))
 
